0c4167e5bfc504f968da788575f82789da4e8c39
baek <bgh9651@gmail.com>
2024-09-11 16:44:03 +0900

    Upt. reviewCard category delete

--- components/inquiry/ReviewCard.tsx
+++ components/inquiry/ReviewCard.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 interface ReviewCardProps {
   name: string;
-  category: string;
   content: string;
 }
 
-const ReviewCard = ({ name, category, content }: ReviewCardProps) => {
+const ReviewCard = ({ name, content }: ReviewCardProps) => {
   return (
     <div className="shadow-xl rounded-lg px-8 py-6">
       <div className="flex gap-2 items-center">
-        <h4 className="text-lg font-bold">{name}</h4>
-        <span className="text-lightgray font-bold text-sm">| {category}</span>
+        <h4 className="text-lg font-bold">{name} 수강생</h4>
       </div>
       <p className="my-2 text-sm">{content}</p>
     </div>
