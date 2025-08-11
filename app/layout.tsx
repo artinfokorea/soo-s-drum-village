985ab4924326dbb0d2358b60ea55d3796d344a1f
백광현 <bgh9651@gmail.com>
2025-08-11 22:44:27 +0900

    fix: naver search key fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,41 @@
-import type { Metadata, Viewport } from "next";
-import "./globals.css";
-import Header from "@/components/layouts/Header";
-import Footer from "@/components/layouts/Footer";
-import { khand } from "@/utils/font";
+import type { Metadata, Viewport } from 'next';
+import './globals.css';
+import Header from '@/components/layouts/Header';
+import Footer from '@/components/layouts/Footer';
+import { khand } from '@/utils/font';
 
 export const viewport: Viewport = {
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  viewportFit: "cover",
+  viewportFit: 'cover',
 };
 
 export const metadata: Metadata = {
-  title: "수 드럼학원",
+  title: '수 드럼학원',
   description:
-    "재밌게 즐기고 신나게 두드릴 생각만 하고 오시면 됩니다. 걱정하시고 두려워하는 부분은 수 드럼학원에서 해결해 드리겠습니다. 재밌는 드럼라이프를 즐기세요",
+    '재밌게 즐기고 신나게 두드릴 생각만 하고 오시면 됩니다. 걱정하시고 두려워하는 부분은 수 드럼학원에서 해결해 드리겠습니다. 재밌는 드럼라이프를 즐기세요',
   openGraph: {
-    title: "수 드럼학원",
+    title: '수 드럼학원',
     description:
-      "재밌게 즐기고 신나게 두드릴 생각만 하고 오시면 됩니다. 걱정하시고 두려워하는 부분은 수 드럼학원에서 해결해 드리겠습니다. 재밌는 드럼라이프를 즐기세요",
-    url: "/logo.png",
-    siteName: "수 드럼학원",
-    locale: "ko-KR",
-    type: "website",
+      '재밌게 즐기고 신나게 두드릴 생각만 하고 오시면 됩니다. 걱정하시고 두려워하는 부분은 수 드럼학원에서 해결해 드리겠습니다. 재밌는 드럼라이프를 즐기세요',
+    url: '/logo.png',
+    siteName: '수 드럼학원',
+    locale: 'ko-KR',
+    type: 'website',
     images: [
       {
-        url: "/logo.png",
+        url: '/logo.png',
         width: 380,
         height: 380,
-        alt: "수 드럼학원",
+        alt: '수 드럼학원',
       },
     ],
   },
 
   verification: {
     other: {
-      "naver-site-verification": ["53dc8c10f1615bc777e32b3db02d96e1cf88b1b2"],
+      'naver-site-verification': ['b730dd5cedb12730014a8c91c78526da1af1e87b'],
     },
   },
 };
@@ -49,7 +49,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${khand.className} touch-manipulation  text-black  `}
-        style={{ backgroundColor: "#ffffff" }}
+        style={{ backgroundColor: '#ffffff' }}
       >
         <Header />
         {children}
